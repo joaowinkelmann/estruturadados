fc5b0c63b2ce7f3a54b898806be553c769f933c5
unknown <hmaster.studio@gmail.com>
2022-08-11 21:55:05 -0300

    ..

---
+++ aula 2/6/Untitled1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+	int x,y,z;
+	
+	x = y = z = 100;
+	
+	printf("%d, %d, %d", x, y, z);
+}
---
+++ aula 2/7/Untitled4.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+	char sexo;
+	char nome[50];
+	int i;
+	int contamasc = 0;
+	int contafem = 0;
+	printf("Insira o nome e sexo de 10 pessoas\n");
+	
+	for(i=0;i<10;i++){
+		printf("Insira o seu nome:");
+		scanf("%s", &nome);
+		printf("Agora insira o seu sexo: (M/F)");
+		scanf("%s", &sexo);
+		
+		if (sexo=='f'){
+			contafem = contafem + 1;
+		}
+		else{
+			contamasc = contamasc + 1;
+		}
+
+	}
+	
+	printf("Existem %d pessoas com o sexo masculino\n", contamasc);
+	printf("E %d pessoas com o sexo feminino", contafem);
+	return 0;
+}
