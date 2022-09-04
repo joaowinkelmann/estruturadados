b10c526660360224229d854dfe93eacb2b63e49f
winkels7 <hmaster.studio@gmail.com>
2022-09-04 20:23:14 -0300

    ex 10 pronto

---
+++ trabalho/10cpy.c
@@ -0,0 +1,43 @@
+//
+//Escolher entre as funções de abrir e fechar arquivos, estruturação de dados em arquivos de texto e arquivos em modo binário para criar o seu algoritmo.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	FILE *pont_texto;
+    char str_entrada[100];
+    char str_saida[200];
+    char c;
+    int vogais = 0;
+	pont_texto = fopen("textousuario.txt", "a");
+
+    if (pont_texto == NULL){
+		printf("Erro ao tentar manipular o arquivo");
+        exit(1);
+	}
+    else{
+        printf("Arquivo inicializado com sucesso\n\n");
+    }
+
+    printf("Insira os dados a serem adicionados no final do arquivo: ");
+    fgets(str_entrada, 100, stdin);
+    fprintf(pont_texto, "%s \n", str_entrada);
+    printf("\n\nArquivo editado com sucesso\n\n");
+    fclose(pont_texto);
+
+    printf("Visualização do arquivo:\n\n");
+    pont_texto = fopen("textousuario.txt", "r");
+
+   while((c = fgetc(pont_texto))!=EOF) {
+        if(c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' || c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U'){
+            vogais++;
+        }
+        putchar(c);
+    }
+
+    printf("\n\n Quantidade de vogais encontrada no arquivo: %d", vogais);
+
+    fclose(pont_texto);
+    return 0;
+}
