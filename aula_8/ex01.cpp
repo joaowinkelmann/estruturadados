19b3f3820ef71094cd2cd49f8db41904f14d3225
unknown <hmaster.studio@gmail.com>
2022-10-06 20:57:52 -0300

    ¨ÁȄUɭʱؔ☻-Ćꎂ㴀

--- aula_8/ex01.cpp
+++ aula_8/ex01.cpp
@@ -4,13 +4,20 @@
 //Listas  Lineares Encadeadas
 //Rotinas de Manipulação Lista Simplesmente Encadeada
 
+//pList -> ponteiro de lista
+
 struct Nodo{
 	int info;
 	struct Nodo *prox; 
+};
+
+void criarLista (struct ListaSimplesEnc *pList){
+	pList -> prim = NULL; //recebe atribuindo (ponteiro pList recebe a variavel 'prim', esta que possui o valor NULL
 }
 
+
 int main(void){
-	struct ListaSimplesEnc minhLista;
+	struct ListaSimplesEnc minhaLista;
 	int valor,  op;
 	
 	criarLista(&minhaLista);
@@ -21,6 +28,25 @@ int main(void){
 		printf("2 - Mostrar lista\n");
 		printf("3 - Sair\n");
 		printf("Opcao? ");
+		scanf("%d", &op);
 		
+		switch(op){
+			case 1: //inserir elementoo no inicio
+				printf("Inserir um valor? ");
+				scanf("%d", &valor);
+				inserirIni(&minhaLista, valor);				
+				break;
+			case 2:  //mostrar lista
+				if(estaVazia(&minhaLista)){
+					printf("Lista vazia");
+				}
+				else{
+					mostrarLista(&minhaLista);
+				}
+				break;
+			case 3: //sair
+				exit(0);
+		}
+			
 	}
 }
