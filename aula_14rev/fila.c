0f2c4317f81e657a1dab6e8c46c3b56fd8cb7217
unknown <hmaster.studio@gmail.com>
2022-11-24 20:45:04 -0300

    aula 14 revisao

---
+++ aula_14rev/fila.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Fila{
+	int capacidade;
+	float *dados;
+	int primeiro;
+	int ultimo;
+	int nItens;
+};
+
+void criarFila( struct Fila *f, int c){
+}
+
+void inserir(struct Fila *f, int v){
+}
+
+int remover(struct Fila *f){
+}
+
+int estaVazia(struct Fila *f){
+}
+
+int estaCheia(struct Fila *f){
+}
+
+void mostrarFila(struct Fila *f){
+}
+
+int main(void){
+}
